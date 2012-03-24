905441a9d85c744c325e82c4ad5840a2c9420c0f
Robert Lail <robert.lail@cph.org>
2012-03-24 17:17:02 -0500

    moved actions to header, extracted helper to create markup for page header, removed unnecessary columns from Projects and Environments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,28 @@ module ApplicationHelper
     html.html_safe
   end
   
+  def header
+    yield PageHeaderBuilder.new(self)
+    "<hr class=\"clear\" />".html_safe
+  end
+  
+end
+
+
+class PageHeaderBuilder
+  
+  def initialize(context)
+    @context = context
+  end
+  
+  delegate :breadcrumbs, :capture, :to => :@context
+  
+  def actions(&block)
+    html_safe "<div class=\"page-actions\">#{capture(&block)}</div>"
+  end
+  
+  def html_safe(html)
+    html.html_safe
+  end
+  
 end
