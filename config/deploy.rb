ac1e39e372be72d6e11ffaaa201a3f6a6fcc6627
Robert Lail <robert.lail@cph.org>
2012-04-10 08:38:43 -0500

    deploy: copy changelog.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,11 +5,16 @@ set :repository, "git://github.com/boblail/changelog.git"
 set :branch, "master"
 
 after "deploy:update_code", "deploy:compile_assets"
+after "deploy:copy_database_yml", "deploy:symlink_config"
 
 namespace :deploy do
   
   task :compile_assets, :roles => :app do
     rake "assets:precompile"
+  end
+  
+  task :symlink_config, :roles => :app do
+    run "ln -nfs #{shared_path}/config/changelog.yml #{release_path}/config/changelog.yml"
   end
   
 end
