d72899589345b509421ac19155b3933ff752d389
Robert Lail <robert.lail@cph.org>
2012-05-07 09:33:52 -0500

    Show an alert when access is unauthorized, don't crash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,21 @@ class ApplicationController < ActionController::Base
   include FreightTrain
   protect_from_forgery
   
+  rescue_from CanCan::AccessDenied do |exception|
+    if current_user
+      redirect_url = request.referrer.blank? ? root_url : :back
+      redirect_to redirect_url, :alert => exception.message
+    else
+      require_login
+    end
+  end
+  
+  def require_login
+    redirect_to new_user_session_path
+  end
+  
+  
+  
   def unfuddle
     @unfuddle ||= Unfuddle.new
   end
