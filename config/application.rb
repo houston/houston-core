2d7b296a6cb9f281c759eab9d3e1bd5ceeb1dcb2
Robert Lail <robert.lail@cph.org>
2012-05-07 10:20:38 -0500

    set up email to work on cphepdev

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,7 @@ module Changelog
     yaml = YAML.load_file(Rails.root.join("config", "changelog.yml").to_s)
     config.title = yaml.fetch("title", "Changelog")
     config.unfuddle = yaml.fetch("unfuddle", {})
+    config.action_mailer.smtp_settings = yaml["smtp"] if yaml["smtp"]
     
     def config.default_environments
       # !todo: read this from a config file
