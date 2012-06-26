9fed92711e16e47a62b8443a154d28dd0e06db19
Robert Lail <robert.lail@cph.org>
2012-06-26 13:50:02 -0500

    deploy: symlink the tmp folder and overwrite the tmp folder created by Capistrano trying to symlink tmp/pids!

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,8 @@ load File.expand_path("~/epic.cap")
 set :repository, "git://github.com/boblail/changelog.git"
 set :branch, "master"
 
+before "bundler:install", "deploy:symlink_config"
 after "deploy:update_code", "deploy:compile_assets"
-after "deploy:copy_database_yml", "deploy:symlink_config"
 
 namespace :deploy do
   
@@ -15,6 +15,7 @@ namespace :deploy do
   
   task :symlink_config, :roles => :app do
     run "ln -nfs #{shared_path}/config/config.rb #{release_path}/config/config.rb"
+    run "rm -rf #{release_path}/tmp"
     run "ln -nfs #{shared_path}/tmp #{release_path}"
   end
   
