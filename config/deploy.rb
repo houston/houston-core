7bcf18d58e07145361a97123594e219dae8c1b7d
Robert Lail <robert.lail@cph.org>
2012-06-26 13:37:33 -0500

    whoops. symlink the right thing!

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ namespace :deploy do
   
   task :symlink_config, :roles => :app do
     run "ln -nfs #{shared_path}/config/config.rb #{release_path}/config/config.rb"
-    run "ln -nfs #{shared_path}/tmp #{release_path}/tmp"
+    run "ln -nfs #{shared_path}/tmp #{release_path}"
   end
   
 end
