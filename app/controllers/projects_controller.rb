448822f052f7ff0b8acf04da06bf8159213e0424
Robert Lail <robert.lail@cph.org>
2012-07-05 21:12:23 -0500

    Projects: sort by name

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.order(:name).all
 
     respond_to do |format|
       format.html # index.html.erb
