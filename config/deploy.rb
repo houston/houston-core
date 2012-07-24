e640565da53a68f8f05b14175b051082e2821279
Robert Lail <robert.lail@cph.org>
2012-07-23 21:08:15 -0500

    [skip] don't precompile assets twice

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,13 +5,8 @@ set :repository, "git://github.com/boblail/changelog.git"
 set :branch, "master"
 
 before "bundler:install", "deploy:symlink_config"
-after "deploy:update_code", "deploy:compile_assets"
 
 namespace :deploy do
-  
-  task :compile_assets, :roles => :app do
-    rake "assets:precompile"
-  end
   
   task :symlink_config, :roles => :app do
     run "ln -nfs #{shared_path}/config/config.rb #{release_path}/config/config.rb"
