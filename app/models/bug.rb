83ee542dbfc09073032bb008f63cab9dd036c74e
Robert Lail <robert.lail@cph.org>
2012-08-25 21:18:59 -0500

    [fix] get the correct range of dates from errbit

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -25,7 +25,7 @@ class Bug
       protocol = Rails.application.config.errbit[:port] == 443 ? "https" : "http"
       root_url = "#{protocol}://#{Rails.application.config.errbit[:host]}"
       path = "#{root_url}/api/v1/problems.json"
-      url = "#{path}?start_date=#{options[:start_date].strftime("%Y-%m-%d")}&end_date=#{options[:start_date].strftime("%Y-%m-%d")}&auth_token=#{Rails.application.config.errbit[:auth_token]}"
+      url = "#{path}?start_date=#{options[:start_date].strftime("%Y-%m-%d")}&end_date=#{options[:end_date].strftime("%Y-%m-%d")}&auth_token=#{Rails.application.config.errbit[:auth_token]}"
       response = Project.benchmark("[errbit] fetch \"#{url}\"") { Faraday.get(url) }
       problems = JSON.load(response.body)
       
