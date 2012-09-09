8e0d7131cae6ac7fc7f8604ef5bdc7eb6ab57dde
Robert Lail <robert.lail@cph.org>
2012-09-08 21:03:44 -0500

    Release Notes: properly escape HTML in commit messages

--- app/helpers/commit_helper.rb
+++ app/helpers/commit_helper.rb
@@ -14,11 +14,11 @@ module CommitHelper
   
   def format_with_feature_bolded(message)
     feature, sentence = message.split(":", 2)
-    sentence ? "<b>#{feature}:</b>#{sentence}".html_safe : message
+    sentence ? "<b>#{h feature}:</b>#{h sentence}".html_safe : message
   end
   
   def format_with_tickets_linked(project, message)
-    message.gsub Commit::TICKET_PATTERN do |match|
+    h(message).gsub Commit::TICKET_PATTERN do |match|
       ticket_number = Commit::TICKET_PATTERN.match(match)[1]
       link_to match, unfuddle_ticket_url(project, ticket_number)
     end.html_safe
