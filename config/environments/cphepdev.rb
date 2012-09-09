3d23f82656045731cc4d6a2143cdbb43d1e572ae
Robert Lail <robert.lail@cph.org>
2012-09-08 20:16:17 -0500

    display full error info in PRI

--- config/environments/cphepdev.rb
+++ config/environments/cphepdev.rb
@@ -6,8 +6,8 @@ Changelog::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  # Full error reports are enabled and caching is turned on
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
