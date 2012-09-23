c58cf85de8f404209d60b083cb777f92dfb80e38
Robert Lail <robert.lail@cph.org>
2012-09-22 21:23:12 -0500

    User: get rid of Tester Wall; replace with more generic stats page, shows tickets for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,50 +75,18 @@ class UsersController < ApplicationController
     binding.pry unless response[0].to_i == 200
     report = response[1]
     group0 = report.fetch("groups", [])[0] || {}
-    tickets = group0.fetch("tickets", [])
+    tickets_for_user = group0.fetch("tickets", [])
     
-    # binding.pry
-    
-    @tickets = tickets.length
     resolutions = %w{invalid duplicate}
-    @invalid_tickets = tickets.select { |ticket| resolutions.member?(ticket["resolution"]) }.length
-    @fixed_tickets = tickets.select { |ticket| ticket["resolution"] == "fixed" }.length
-    @percent = 100.0 / @tickets
+    invalid_tickets = tickets_for_user.select { |ticket| resolutions.member?(ticket["resolution"]) }.length
+    fixed_tickets = tickets_for_user.select { |ticket| ticket["resolution"] == "fixed" }.length
+    percent = 100.0 / tickets_for_user.length
     
-    
-    
-    # respond_to do |format|
-    #   format.html do
-    #     
-    #     template = "show"
-    #     
-    #     if @user.tester?
-    #       template = "tester_wall"
-    #       tickets_in_testing = Ticket.in_queues "in_testing", "in_testing_production"
-    #       tickets_tested_by_user = tickets_in_testing.with_testing_notes_by(@user)
-    #       
-    #       # These are tickets that are:
-    #       #  1. In Testing
-    #       #  2. Where the tester's most recent note is failing
-    #       #  3. Where the tester's most recent note is before a release
-    #       @tickets_to_retest = tickets_tested_by_user.to_be_retested_by(@user)
-    #       
-    #       # These are tickets that are:
-    #       #  1. In Testing
-    #       #  2. Where the tester hasn't created any notes
-    #       @tickets_to_test = tickets_in_testing.without_testing_notes_by(@user)
-    #       
-    #       # These are tickets that are:
-    #       #  1. In Testing
-    #       #  2. Where the tester _has_ created a note
-    #       #  3. That are not in @tickets_to_retest
-    #       @tickets_already_tested = tickets_tested_by_user - @tickets_to_retest
-    #     end
-    #     
-    #     render template: "users/#{template}"
-    #   end
-    #   format.json { render json: @user }
-    # end
+    @stats = {
+      tickets: tickets_for_user.length,
+      invalid_tickets: invalid_tickets * percent,
+      fixed_tickets: fixed_tickets * percent
+    }
   end
 
   # GET /users/new
