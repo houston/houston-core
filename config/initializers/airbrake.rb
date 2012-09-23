dbbeed6a1f07337878cbf6ef41db7902d73f0df7
Robert Lail <robert.lail@cph.org>
2012-09-23 16:22:42 -0500

    [refactor] move Airbrake configuration do config.rb

--- config/initializers/airbrake.rb
+++
@@ -1,6 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key  = '8ca3055643a8dc96a9bcea57f8ad0dee'
-  config.host     = 'errbit.cphepdev.com'
-  config.port     = 80
-  config.secure   = config.port == 443
-end
