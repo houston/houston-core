d1a267dc4abf14af605ab58480aef228ce4663e4
Robert Lail <robert.lail@cph.org>
2012-11-06 18:58:03 -0600

    update deploy tasks (fix setup experience)

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,9 +2,10 @@ set :application, "houston"
 load File.expand_path("~/epic.cap")
 
 set :repository, "git://github.com/houstonmc/houston.git"
-set :branch, "master"
 
 before "bundler:install", "deploy:symlink_config"
+after "deploy:setup", "deploy:copy_config_rb"
+after "deploy:setup", "deploy:create_shared_folders"
 
 namespace :deploy do
   
@@ -12,6 +13,17 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/config/config.rb #{release_path}/config/config.rb"
     run "rm -rf #{release_path}/tmp"
     run "ln -nfs #{shared_path}/tmp #{release_path}"
+  end
+  
+  desc "Copy config.rb"
+  task :copy_config_rb, :roles => :app do
+    run "mkdir -p #{shared_path}/config"
+    put(File.read(File.join(Dir.pwd, "config/config.rb")), "#{shared_path}/config/config.rb")
+  end
+  
+  desc "Create other shared folders"
+  task :create_shared_folders, :roles => :app do
+    run "mkdir -p #{shared_path}/tmp"
   end
   
 end
