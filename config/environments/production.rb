8d8e86e36dd4803b9fcd323bd475682dd66b003a
Robert Lail <robert.lail@cph.org>
2012-11-08 13:40:50 -0600

    update production configuration file to send emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,9 @@ Houston::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # So we can send emails
+  # !todo: pull this out into Houston's config file!
+  config.action_mailer.default_url_options = { host: "status.cphepdev.com" }
+  
 end
