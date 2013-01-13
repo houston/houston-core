2dab4ee27514ffb4194c8347baac368bb6948800
Robert Lail <robert.lail@cph.org>
2013-01-13 16:29:54 -0600

    [skip] remove create-job logic from Jenkins adapter

--- app/models/houston/ci/adapter/jenkins_adapter/job.rb
+++ app/models/houston/ci/adapter/jenkins_adapter/job.rb
@@ -17,22 +17,8 @@ module Houston
             url = build_url(commit)
             Rails.logger.info "[jenkins] POST #{url}"
             response = Faraday.post(url)
-            if response.status == 404
-              
-              unless Faraday.get(host).status == 200
-                raise Houston::CI::Error.new("Houston was looking for an instance of Jenkins at #{host}, but it could not find Jenkins at that URL.")
-              end
-              
-              Rails.logger.info "[jenkins] Attempting to create a job on #{host} for #{project.slug}"
-              create_job_or_fail!
-              
-              # Retry once
-              Rails.logger.info "[jenkins] POST #{url}"
-              response = Faraday.post(url)
-              
-              unless response.status == 200
-                raise Houston::CI::Error.new("Houston attempted to create a job named \"#{project.slug}\" at #{host}, but it was unable to do so.")
-              end
+            unless response.status == 200
+              raise Houston::CI::Error.new("Houston was unable to trigger a build for #{project.name} with the URL #{url}.")
             end
           end
           
@@ -125,19 +111,6 @@ module Houston
           end
           
           
-          
-          def create_job_or_fail!
-            client = Jenkins::Client.new
-            client.url = host
-            
-            template = ERB.new(File.read(Rails.root.join("config.xml.erb")))
-            git_url = project.version_control_location
-            config = template.result(binding)
-            
-            job = Jenkins::Client::Job.new(name: project.slug, client: client)
-            success = job.create!(config)
-            raise Houston::CI::Error unless success
-          end
           
           def job_url
             "#{host}/job/#{project.slug}"
