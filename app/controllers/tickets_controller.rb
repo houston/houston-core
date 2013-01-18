21064aa08df51841b8b96bfcfffc23f983cb325f
Robert Lail <robert.lail@cph.org>
2013-01-18 16:54:59 -0600

    [fix] fix regressions with Unfuddle gem

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,13 +4,13 @@ class TicketsController < ApplicationController
     @tickets = UnfuddleDump.load!
     @last_updated = UnfuddleDump.last_updated
     
-    users = Unfuddle.instance.get("people.json?removed=true")[1]
+    users = Unfuddle.instance.get("people.json?removed=true").json
     @user_names_by_ids = {}
     users.each do |user|
       @user_names_by_ids[user["id"]] = "#{user["first_name"].strip} #{user["last_name"].strip}"
     end
     
-    projects = Unfuddle.instance.get("projects.json?removed=true")[1]
+    projects = Unfuddle.instance.get("projects.json?removed=true").json
     @project_names_by_ids = {}
     projects.each do |project|
       @project_names_by_ids[project["id"]] = project["title"]
