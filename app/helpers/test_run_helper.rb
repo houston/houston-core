619c9399ad2a194c3e389bbd8dcf5ef4b7bf53ec
Robert Lail <robert.lail@cph.org>
2013-02-20 07:58:12 -0600

    [skip] show a pending state for test runs

--- app/helpers/test_run_helper.rb
+++ app/helpers/test_run_helper.rb
@@ -3,7 +3,8 @@ module TestRunHelper
   def test_run_summary(test_run)
     subject = {
       "pass" => "#{test_run.total_count} tests passed!",
-      "fail" => "#{test_run.fail_count} of #{test_run.total_count} tests failed"
+      "fail" => "#{test_run.fail_count} of #{test_run.total_count} tests failed",
+      "" => "started #{distance_of_time_in_words(test_run.created_at, Time.now)} ago"
     }[test_run.result.to_s]
   end
   
