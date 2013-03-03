c42da5245c97bc36920d8d865b9fe30ff4d03d79
Robert Lail <robert.lail@cph.org>
2013-03-03 17:24:05 -0600

    [refactor] clean up users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   before_filter :extract_administrator, :only => [:update, :create]
   load_and_authorize_resource
   
-  # GET /users
-  # GET /users.json
+  
   def index
     @title = "Users"
     
@@ -39,15 +38,9 @@ class UsersController < ApplicationController
         tickets_by_severity: tickets_by_severity
       }
     end
-    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
   end
-
-  # GET /users/1
-  # GET /users/1.json
+  
+  
   def show
     @user = User.find(params[:id])
     @title = @user.name
@@ -84,31 +77,25 @@ class UsersController < ApplicationController
       
     end
   end
-
-  # GET /users/new
-  # GET /users/new.json
+  
+  
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
   end
-
-  # GET /users/1/edit
+  
+  
   def edit
     @user = User.find(params[:id])
   end
-
+  
+  
   def invite
     @user = User.find(params[:id])
     @user.invite!
     redirect_to request.referrer, :notice => "#{@user.name} has been invited to use this program"
   end
-
-  # POST /users
-  # POST /users.json
+  
+  
   def create
     if params[:send_invitation]
       @user = User.invite!(params[:user])
@@ -122,44 +109,31 @@ class UsersController < ApplicationController
     
     @user.administrator = @administrator
     
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully invited.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to @user, notice: 'User was successfully invited.'
+    else
+      render action: "new"
     end
   end
-
-  # PUT /users/1
-  # PUT /users/1.json
+  
+  
   def update
     @user = User.find(params[:id])
     @user.administrator = @administrator
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    
+    if @user.update_attributes(params[:user])
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render action: "edit"
     end
   end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
+  
+  
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+    
+    redirect_to users_url
   end
   
   
