925e41cca8552f732dfe7152c24e28f0eb7afe1e
Robert Lail <robert.lail@cph.org>
2013-03-02 19:53:41 -0600

    [skip] clean up projects_controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,80 +3,56 @@ class ProjectsController < ApplicationController
   load_resource :find_by => :slug # will use find_by_permalink!(params[:id])
   authorize_resource
   
-  # GET /projects
-  # GET /projects.json
+  
   def index
     @title = "Projects"
     @projects = Project.all
-    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @projects }
-    end
   end
-
-  # GET /projects/1
-  # GET /projects/1.json
+  
+  
   def show
     redirect_to project_dashboard_path(@project)
   end
-
-  # GET /projects/new
-  # GET /projects/new.json
+  
+  
   def new
     @title = "New Project"
     
     @project = Project.new
     @project.maintainers << current_user if @project.maintainers.none?
-    
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @project }
-    end
   end
-
-  # GET /projects/1/edit
+  
+  
   def edit
     @project = Project.find_by_slug!(params[:id])
     @project.maintainers << current_user if @project.maintainers.none?
     
     @title = "#{@project.name}: Edit"
   end
-
-  # POST /projects
-  # POST /projects.json
+  
+  
   def create
     @project = Project.new(params[:project])
-
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to projects_path, notice: 'Project was successfully created.' }
-        format.json { render json: @project, status: :created, location: @project }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    
+    if @project.save
+      redirect_to projects_path, notice: 'Project was successfully created.'
+    else
+      render action: "new"
     end
   end
-
-  # PUT /projects/1
-  # PUT /projects/1.json
+  
+  
   def update
     @project = Project.find_by_slug!(params[:id])
     
-    respond_to do |format|
-      if @project.update_attributes(params[:project])
-        format.html { redirect_to projects_path, notice: 'Project was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.update_attributes(params[:project])
+      redirect_to projects_path, notice: 'Project was successfully updated.'
+    else
+      render action: "edit"
     end
   end
   
   
-  # PUT /projects/1/retire
   def retire
     @project = Project.find_by_slug!(params[:id])
     @project.retire!
@@ -84,16 +60,11 @@ class ProjectsController < ApplicationController
   end
   
   
-  # DELETE /projects/1
-  # DELETE /projects/1.json
   def destroy
     @project = Project.find_by_slug!(params[:id])
     @project.destroy
     
-    respond_to do |format|
-      format.html { redirect_to projects_url }
-      format.json { head :no_content }
-    end
+    redirect_to projects_url
   end
   
   
