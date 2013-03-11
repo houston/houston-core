342314c4498460c633084bd10a0b68d07ae15728
Robert Lail <robert.lail@cph.org>
2013-03-10 20:37:31 -0500

    [improvement] update jQuery UI and houston-scheduler

--- vendor/assets/javascripts/jquery-ui.js
+++ vendor/assets/javascripts/jquery-ui.js
@@ -5,15 +5,15 @@
 // the compiled file.
 //
 //= require ./jquery-ui/jquery.ui.core.js
-//  require ./jquery-ui/jquery.ui.widget.js
-//  require ./jquery-ui/jquery.ui.mouse.js
+//= require ./jquery-ui/jquery.ui.widget.js
+//= require ./jquery-ui/jquery.ui.mouse.js
 //  require ./jquery-ui/jquery.ui.position.js
 //
 //  require ./jquery-ui/jquery.ui.draggable.js
 //  require ./jquery-ui/jquery.ui.droppable.js
 //  require ./jquery-ui/jquery.ui.resizable.js
 //  require ./jquery-ui/jquery.ui.selectable.js
-//  require ./jquery-ui/jquery.ui.sortable.js
+//= require ./jquery-ui/jquery.ui.sortable.js
 //
 //  require ./jquery-ui/jquery.ui.accordian.js
 //  require ./jquery-ui/jquery.ui.autocomplete.js
@@ -24,17 +24,32 @@
 //  require ./jquery-ui/jquery.ui.datepicker.js
 //  require ./jquery-ui/jquery.ui.progressbar.js
 //
-//= require ./jquery-ui/jquery.effects.core.js
+//  require ./jquery-ui/jquery.effects.core.js
 //  require ./jquery-ui/jquery.effects.blind.js
 //  require ./jquery-ui/jquery.effects.bounce.js
 //  require ./jquery-ui/jquery.effects.clip.js
 //  require ./jquery-ui/jquery.effects.drop.js
 //  require ./jquery-ui/jquery.effects.explode.js
-//= require ./jquery-ui/jquery.effects.fade.js
+//  require ./jquery-ui/jquery.effects.fade.js
 //  require ./jquery-ui/jquery.effects.fold.js
-//= require ./jquery-ui/jquery.effects.highlight.js
+//  require ./jquery-ui/jquery.effects.highlight.js
 //  require ./jquery-ui/jquery.effects.pulsate.js
 //  require ./jquery-ui/jquery.effects.scale.js
 //  require ./jquery-ui/jquery.effects.shake.js
-//= require ./jquery-ui/jquery.effects.slide.js
+//  require ./jquery-ui/jquery.effects.slide.js
 //  require ./jquery-ui/jquery.effects.transfer.js
+//
+//= require ./jquery-ui/jquery.ui.effect.js
+//  require ./jquery-ui/jquery.ui.effect-blind.js
+//  require ./jquery-ui/jquery.ui.effect-bounce.js
+//  require ./jquery-ui/jquery.ui.effect-clip.js
+//  require ./jquery-ui/jquery.ui.effect-drop.js
+//  require ./jquery-ui/jquery.ui.effect-explode.js
+//= require ./jquery-ui/jquery.ui.effect-fade.js
+//  require ./jquery-ui/jquery.ui.effect-fold.js
+//= require ./jquery-ui/jquery.ui.effect-highlight.js
+//  require ./jquery-ui/jquery.ui.effect-pulsate.js
+//  require ./jquery-ui/jquery.ui.effect-scale.js
+//  require ./jquery-ui/jquery.ui.effect-shake.js
+//= require ./jquery-ui/jquery.ui.effect-slide.js
+//  require ./jquery-ui/jquery.ui.effect-transfer.js
