cde13eaf025c61b8351f187a1f5321143bbe5352
Robert Lail <robert.lail@cph.org>
2013-04-25 19:39:37 -0500

    [refactor] refactor TicketPresenter a little farther to reduce duplication

--- app/presenters/ticket_presenter.rb
+++ app/presenters/ticket_presenter.rb
@@ -23,54 +23,39 @@ class TicketPresenter
   end
   
   def as_json(*args)
-    @tickets.map do |ticket|
-      project = ticket.project
-      { id: ticket.id,
-        projectId: project.ticket_tracker_id,
-        projectSlug: project.slug,
-        projectTitle: project.name,
-        projectColor: project.color,
-        number: ticket.number,
-        summary: ticket.summary,
-        verdict: ticket.verdict.downcase,
-        verdictsByTester: ticket.verdicts_by_tester_index,
-        queue: ticket.queue,
-        committers: ticket.committers,
-        deployment: ticket.deployment,
-        age: ticket.age,
-        dueDate: ticket.due_date,
-        ticketSystem: project.ticket_tracker_adapter,
-        ticketUrl: ticket.ticket_tracker_ticket_url }
-    end
+    @tickets.map(&method(:ticket_to_json))
+  end
+  
+  def ticket_to_json(ticket)
+    project = ticket.project
+    { id: ticket.id,
+      projectId: project.ticket_tracker_id,
+      projectSlug: project.slug,
+      projectTitle: project.name,
+      projectColor: project.color,
+      number: ticket.number,
+      summary: ticket.summary,
+      verdict: ticket.verdict.downcase,
+      verdictsByTester: ticket.verdicts_by_tester_index,
+      queue: ticket.queue,
+      committers: ticket.committers,
+      deployment: ticket.deployment,
+      age: ticket.age,
+      dueDate: ticket.due_date,
+      ticketSystem: project.ticket_tracker_adapter,
+      ticketUrl: ticket.ticket_tracker_ticket_url }
   end
   
   def with_testing_notes
     @tickets.map do |ticket|
-      project = ticket.project
-      { id: ticket.id,
-        projectId: project.ticket_tracker_id,
-        projectSlug: project.slug,
-        projectTitle: project.name,
-        projectColor: project.color,
-        projectMaintainers: project.maintainers_ids,
-        number: ticket.number,
-        summary: ticket.summary,
-        verdict: ticket.verdict.downcase,
-        verdictsByTester: ticket.verdicts_by_tester_index,
-        queue: ticket.queue,
-        committers: ticket.committers,
-        deployment: ticket.deployment,
-        age: ticket.age,
-        dueDate: ticket.due_date,
-        ticketSystem: project.ticket_tracker_adapter,
-        ticketUrl: ticket.ticket_tracker_ticket_url,
-        
+      ticket_to_json(ticket).merge({
+        projectMaintainers: ticket.project.maintainers_ids,
         minPassingVerdicts: ticket.min_passing_verdicts,
         testingNotes: TestingNotePresenter.new(ticket.testing_notes).as_json,
         commits: CommitPresenter.new(ticket.commits).as_json,
         releases: ReleasePresenter.new(ticket.releases).as_json,
         lastReleaseAt: ticket.last_release_at,
-        description: mdown(ticket.description) }
+        description: mdown(ticket.description) })
     end
   end
   
