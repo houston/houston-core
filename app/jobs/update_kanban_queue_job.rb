5c8c189013f911ce5dcb9063abfb51f76d7105da
Robert Lail <robert.lail@cph.org>
2013-05-05 16:18:19 -0500

    [skip] Corrected KanbanQueueJob to only work for projects that use Unfuddle

--- app/jobs/update_kanban_queue_job.rb
+++ app/jobs/update_kanban_queue_job.rb
@@ -9,7 +9,7 @@ class UpdateKanbanQueueJob
   end
   
   def run!
-    Project.with_ticket_tracker.each do |project|
+    Project.where(ticket_tracker_name: "Unfuddle").each do |project|
       update_tickets_for_project!(project)
     end
   rescue QuitAll
