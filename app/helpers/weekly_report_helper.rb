dc8ff0f2e686cd5113cc0a70068f13b0086565af
Bob Lail <bob.lail@cph.org>
2013-05-26 14:40:13 -0500

    [refactor] Extracted next_week and prev_week from Weekly Report template

--- app/helpers/weekly_report_helper.rb
+++ app/helpers/weekly_report_helper.rb
@@ -4,4 +4,12 @@ module WeeklyReportHelper
     year == Date.today.year
   end
   
+  def next_week(weekly_report)
+    1.week.since(weekly_report.date_range.begin)
+  end
+  
+  def prev_week(weekly_report)
+    1.week.until(weekly_report.date_range.begin)
+  end
+  
 end
