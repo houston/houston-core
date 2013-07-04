e21c42c7466f7d1f08b23791cda291f118ce9e69
Bob Lail <bob.lail@cph.org>
2013-07-04 16:01:17 -0500

    [refactor] Pulled TestingNote and Deploy notification handlers out of initializers and into config.rb to give finer-grained control over how they work

--- config/initializers/notify_committers_of_failing_verdict.rb
+++
@@ -1,5 +0,0 @@
-Houston.observer.on "testing_note:save" do |testing_note|
-  unless testing_note.verdict == "works"
-    ProjectNotification.testing_note(testing_note).deliver!
-  end
-end
--- config/initializers/notify_maintainers_of_deploy.rb
+++
@@ -1,5 +0,0 @@
-Houston.observer.on "deploy:create" do |deploy|
-  deploy.project.maintainers.each do |maintainer|
-    ProjectNotification.maintainer_of_deploy(maintainer, deploy).deliver!
-  end
-end
