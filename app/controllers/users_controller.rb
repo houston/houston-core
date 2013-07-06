acdb73e85ae3bf78416b38fdaf7adb39d006daf1
Bob Lail <bob.lail@cph.org>
2013-07-06 09:27:32 -0500

    [fix] Allowed the user view to load when Unfuddle is unavailable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,37 +43,36 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @title = @user.name
+    @stats = {
+      tickets: 0,
+      invalid_tickets: 1/0.0, # NaN
+      fixed_tickets: 1/0.0 } # NaN
     
     if @user.unfuddle_id
+      begin
+        url = "ticket_reports/dynamic.json"
+        url << "?conditions_string=reporter-eq-#{@user.unfuddle_id}"
+        url << "&fields_string=#{%w{project number summary resolution}.join("|")}"
+        url << "&pretty=true&exclude_description=true"
+        response = Unfuddle.get(url)
       
-      url = "ticket_reports/dynamic.json"
-      url << "?conditions_string=reporter-eq-#{@user.unfuddle_id}"
-      url << "&fields_string=#{%w{project number summary resolution}.join("|")}"
-      url << "&pretty=true&exclude_description=true"
-      response = Unfuddle.get(url)
+        binding.pry unless response.status == 200
+        report = response.json
+        group0 = report.fetch("groups", [])[0] || {}
+        tickets_for_user = group0.fetch("tickets", [])
       
-      binding.pry unless response.status == 200
-      report = response.json
-      group0 = report.fetch("groups", [])[0] || {}
-      tickets_for_user = group0.fetch("tickets", [])
+        resolutions = %w{invalid duplicate}
+        invalid_tickets = tickets_for_user.select { |ticket| resolutions.member?(ticket["resolution"]) }.length
+        fixed_tickets = tickets_for_user.select { |ticket| ticket["resolution"] == "fixed" }.length
+        percent = 100.0 / tickets_for_user.length
       
-      resolutions = %w{invalid duplicate}
-      invalid_tickets = tickets_for_user.select { |ticket| resolutions.member?(ticket["resolution"]) }.length
-      fixed_tickets = tickets_for_user.select { |ticket| ticket["resolution"] == "fixed" }.length
-      percent = 100.0 / tickets_for_user.length
+        @stats = {
+          tickets: tickets_for_user.length,
+          invalid_tickets: invalid_tickets * percent,
+          fixed_tickets: fixed_tickets * percent }
       
-      @stats = {
-        tickets: tickets_for_user.length,
-        invalid_tickets: invalid_tickets * percent,
-        fixed_tickets: fixed_tickets * percent }
-      
-    else
-      
-      @stats = {
-        tickets: 0,
-        invalid_tickets: 1/0.0, # NaN
-        fixed_tickets: 1/0.0 } # NaN
-      
+      rescue Unfuddle::ConnectionError
+      end
     end
   end
   
