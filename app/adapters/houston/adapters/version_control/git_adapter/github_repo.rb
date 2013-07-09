ec38857f37e8f96160186833b677e2f48b5cb4f5
Bob Lail <bob.lail@cph.org>
2013-07-09 12:49:13 -0500

    [skip] Tweaked formatting and spaces

--- app/adapters/houston/adapters/version_control/git_adapter/github_repo.rb
+++ app/adapters/houston/adapters/version_control/git_adapter/github_repo.rb
@@ -6,13 +6,16 @@ module Houston
           
           
           def project_url
-            location.to_s.gsub(/^git@(?:www\.)?github.com:/, "https://github.com/").gsub(/^git:/, "https:").gsub(/\.git$/, "")
+            location.to_s
+              .gsub(/^git@(?:www\.)?github.com:/, "https://github.com/")
+              .gsub(/^git:/, "https:")
+              .gsub(/\.git$/, "")
           end
-
+          
           def commit_url(sha)
             "#{project_url}/commit/#{sha}"
           end
-
+          
           def commit_range_url(sha0, sha1)
             "#{project_url}/compare/#{sha0}...#{sha1}"
           end
