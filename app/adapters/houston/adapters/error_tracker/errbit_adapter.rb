e66d589ab0dbdf39e92298cb763dfeb05bd28704
Bob Lail <bob.lail@cph.org>
2013-12-15 19:19:51 -0600

    [skip] Allowed Errbit IDs to be normal integers (2m)

--- app/adapters/houston/adapters/error_tracker/errbit_adapter.rb
+++ app/adapters/houston/adapters/error_tracker/errbit_adapter.rb
@@ -7,7 +7,6 @@ module Houston
           
           def errors_with_parameters(project, app_id)
             return {errbit_app_id: ["cannot be blank"]} if app_id.blank?
-            return {errbit_app_id: ["must be a hexadecimal number at least 24 characters long"]} unless app_id.to_s =~ /^[\da-f]{24,}$/
             
             # !todo: validate that the app exists
             # begin
