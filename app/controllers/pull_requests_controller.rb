d8e7d99b0044e5049ba9d4eed0ac5532f80afa2c
Bob Lail <bob.lail@cph.org>
2014-01-09 22:08:57 -0600

    [fix] Require a user to be logged in to view the PullRequests page (3m) {{err:8688}}

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,4 +1,5 @@
 class PullRequestsController < ApplicationController
+  before_filter :authenticate_user!
   
   rescue_from Github::Unauthorized do |exception|
     redirect_to oauth_consumer_path(id: "github")
