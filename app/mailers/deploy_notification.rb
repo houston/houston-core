0533ed3b0300f989fea0a64304b22684de2c0c3d
Bob Lail <bob.lail@cph.org>
2014-01-14 19:46:53 -0600

    [refactor] Introduced DeployNotification to assist in notifying the deployer when that person is not a maintainer (5m)

---
+++ app/mailers/deploy_notification.rb
@@ -0,0 +1,31 @@
+class DeployNotification
+  
+  def initialize(deploy)
+    @deploy = deploy
+    @deployer = deploy.deployer
+    @maintainers = deploy.project.maintainers
+  end
+  
+  attr_reader :deploy, :deployer, :maintainers
+  
+  def deliver!
+    messages = maintainers.map do |maintainer|
+      ProjectNotification.maintainer_of_deploy(maintainer, deploy).deliver!
+    end
+    
+    if we_know_who_triggered_the_deploy and it_wasnt_a_maintainer
+      messages << ProjectNotification.maintainer_of_deploy(deployer, deploy).deliver!
+    end
+    
+    messages
+  end
+  
+  def we_know_who_triggered_the_deploy
+    !deployer.blank?
+  end
+  
+  def it_wasnt_a_maintainer
+    !maintainers.map(&:email).member?(deployer)
+  end
+  
+end
