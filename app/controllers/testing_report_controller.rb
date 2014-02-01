f8a5be776d25b8f476372496ab3da45906306242
Bob Lail <bob.lail@cph.org>
2014-01-31 18:29:13 -0600

    [refactor] Rewrote the Testing Report to not hard-code Unfuddle as a requirement; sped up results; omitted retired projects (20m)

--- app/controllers/testing_report_controller.rb
+++ app/controllers/testing_report_controller.rb
@@ -1,42 +1,43 @@
 class TestingReportController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :find_project, only: [:show]
   
   
   def index
     @title = "Testing Report"
     
-    # Faster; one fetch to Unfuddle for all projects
-    unfuddle_tickets = Unfuddle.instance.find_tickets!(status: :resolved, resolution: :fixed)
-    
-    @tickets = []
-    @projects = Project.where(ticket_tracker_name: "Unfuddle")
-    
-    @projects.each do |project|
-      next unless can?(:show, project.testing_notes.build)
-      
-      tickets_for_project = unfuddle_tickets
-        .select { |ticket| ticket["project_id"].to_s == project.extended_attributes["unfuddle_project_id"] }
-        .map { | attributes| project.ticket_tracker.build_ticket(attributes) }
-      tickets = project.tickets
-        .includes(:testing_notes)
-        .includes(:commits)
-        .synchronize(tickets_for_project)
-        .reject(&:in_development?)
-      
-      @tickets.concat tickets
-    end
-    
-    @tickets = TicketPresenter.new(@tickets).with_testing_notes
-    render json: @tickets if request.xhr?
+    @projects = Project.unretired.select { |project| can?(:read, project.testing_notes.build) }
+    @tickets = Ticket.for_projects @projects
   end
   
   
   def show
-    @project = Project.find_by_slug!(params[:slug])
     @title = "Testing Report: #{@project.name}"
     authorize! :show, @project.testing_notes.build
     
+    @projects = [@project]
     @tickets = @project.tickets
+  end
+  
+  
+private
+  
+  
+  def find_project
+    @project = Project.find_by_slug!(params[:slug])
+  end
+  
+  
+  def default_render
+    @tickets = tickets_for_testing_report(@tickets)
+    @tickets = TicketPresenter.new(@tickets).with_testing_notes
+    render json: @tickets if request.xhr?
+    super
+  end
+  
+  
+  def tickets_for_testing_report(tickets)
+    tickets
       .unclosed
       .fixed
       .deployed
@@ -44,10 +45,7 @@ class TestingReportController < ApplicationController
       .includes(:testing_notes)
       .includes(:releases)
       .includes(:commits)
-    @projects = [@project]
-    
-    @tickets = TicketPresenter.new(@tickets).with_testing_notes
-    render json: @tickets if request.xhr?
+      .order("projects.name ASC")
   end
   
   
