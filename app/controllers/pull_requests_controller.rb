a0b800a221e22e89f0bc68289f7296c9a4fa08e1
Bob Lail <bob.lail@cph.org>
2014-02-08 14:12:03 -0600

    [skip] Titled the Pull Requests view (1m)

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -2,6 +2,7 @@ class PullRequestsController < ApplicationController
   before_filter :authenticate_user!
   
   def index
+    @title = "Pull Requests"
     @pull_requests_by_repo = Github::PullRequests.new(current_user).to_h
   end
   
