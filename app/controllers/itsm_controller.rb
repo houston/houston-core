0c1b91d51db7e1b2d21ef88ac1f3b944f2c2fbd2
Bob Lail <bob.lail@cph.org>
2014-02-26 08:42:30 -0600

    [fix] Handled the case where there are zero ITSMs (10m)

--- app/controllers/itsm_controller.rb
+++ app/controllers/itsm_controller.rb
@@ -7,11 +7,17 @@ class ItsmController < ApplicationController
     req = Net::HTTP::Get.new("/ITSM.asmx/GetOpenCallsEmergingProducts")
     req.ntlm_auth("Houston", "cph.pri", "gKfub6mFy9BHDs6")
     response = http.request(req)
-    @issues = Hash.from_xml(response.body)["ArrayOfOpenCallData"]["OpenCallData"].map do |issue|
-      issue.merge("CallDetailUrl" => Nokogiri::HTML::fragment(issue["CallDetailLink"]).children.first[:href])
+    issues = Hash.from_xml(response.body).fetch("ArrayOfOpenCallData").fetch("OpenCallData", [])
+    @issues = issues.map do |issue|
+      url = Nokogiri::HTML::fragment(issue["CallDetailLink"]).children.first[:href]
+      user = User.find_by_email(issue["AssignedToEmailAddress"].downcase)
+      Issue.new(issue["Summary"], url, issue["AssignedTo"], user)
     end
     
     render partial: "itsm/fires" if request.xhr?
   end
+  
+  
+  Issue = Struct.new(:summary, :url, :assigned_to, :assigned_to_user)
   
 end
