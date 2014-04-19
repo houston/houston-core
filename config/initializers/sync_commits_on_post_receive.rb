8cb6bf89c329bbf6d6f9aec864587a794028e3d3
Bob Lail <bob.lail@cph.org>
2014-04-19 13:33:24 -0500

    [skip] Tried publishing Gemfiles to Gemnasium on post-receive (5m)

--- config/initializers/sync_commits_on_post_receive.rb
+++ config/initializers/sync_commits_on_post_receive.rb
@@ -1,3 +1,8 @@
 Houston.observer.on "hooks:post_receive" do |project, params|
-  project.repo.refresh!(async: true)
+  project.repo.refresh!
+  
+  payload = PostReceivePayload.new(params)
+  if payload.branch == project.gemnasium_branch && !project.gemnasium_slug.blank?
+    PushGemfileToGemnasium.new(project).perform
+  end
 end
