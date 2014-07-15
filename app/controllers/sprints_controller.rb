8f159c502363b5852fa45794b11df04779f54e08
Bob Lail <bob.lail@cph.org>
2014-07-14 21:04:56 -0500

    [improvement] Allowed adding tasks marked "unable to estimate" into a sprint (adding the task removes the flag) (5m)

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -7,16 +7,13 @@ class SprintsController < ApplicationController
   
   def current
     @sprint = Sprint.current || Sprint.create!
-    redirect_to @sprint
+    redirect_to sprint
   end
   
   
   def show
     authorize! :read, sprint
-    @open_tasks = Task
-      .joins(:ticket)
-      .joins("INNER JOIN projects ON tickets.project_id=projects.id")
-      .merge(Ticket.open.able_to_estimate)  # <-- knows about Houston scheduler
+    @open_tasks = Task.joins(:ticket => :project).merge(Ticket.open)
     @tasks = @sprint.tasks.includes(:checked_out_by)
     render template: "sprints/show"
   end
@@ -32,6 +29,10 @@ class SprintsController < ApplicationController
   def add_task
     authorize! :update, sprint
     task = Task.find(params[:task_id])
+    
+    # Putting a task into a Sprint implies that you're able to estimate this ticket
+    task.ticket.able_to_estimate! if task.ticket.respond_to?(:able_to_estimate!)
+    
     if task.completed?
       render text: "Task ##{task.shorthand} cannot be added to the Sprint because it has been completed",
         status: :unprocessable_entity
