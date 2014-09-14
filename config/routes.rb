50d4b1fa98a3d12541caa19fc2c993a2edb2d248
Bob Lail <bob.lail@cph.org>
2014-09-14 13:35:00 -0500

    [skip] Deleted unused route (1m)

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :sessions => "sessions" }
   
   root to: "welcome#index", via: :get
-  get "tdl" => "welcome#tdl"
   get "activity" => "welcome#activity"
   
   
