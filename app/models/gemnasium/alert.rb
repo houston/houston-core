e45c3cc03ba32ac9ca536c9c4840244238690ea8
Bob Lail <bob.lail@cph.org>
2014-10-19 19:07:21 -0500

    [fix] Logged errors communicating with Gemnasium rather than raising them {{err:7102}} (5m)

--- app/models/gemnasium/alert.rb
+++ app/models/gemnasium/alert.rb
@@ -7,18 +7,24 @@ module Gemnasium
       
       response = connection.get "projects"
       unless response.status == 200
-        Rails.logger.error "\e[31m GET /projects responded with #{response.status}\e[0m"
+        Rails.logger.warn "\e[31m[gemnasium] \e[1mGET /projects\e[0;31m responded with #{response.status}\e[0m"
         return
       end
       
       projects = JSON.load(response.body).values.flatten
       
       projects.parallel.map do |project|
-        response = connection.get "projects/#{project["slug"]}/alerts"
+        path = "projects/#{project["slug"]}/alerts"
+        response = connection.get path
+        unless response.status == 200
+          Rails.logger.warn "\e[31m[gemnasium] \e[1mGET /#{path}\e[0;31m responded with #{response.status}\e[0m"
+          next
+        end
+        
         Array(JSON.load(response.body)).map { |alert| alert.merge(
           "project_id" => project["slug"],
           "project_slug" => project["name"]) }
-      end.flatten
+      end.flatten.compact
     end
     
     def self.open
