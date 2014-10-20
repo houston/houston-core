b903a4af51600babef3f29a4e69a430a7ba717b7
Bob Lail <bob.lail@cph.org>
2014-10-19 19:29:17 -0500

    [skip] Used a lockfile with Rufus::Scheduler and logged the scheduler's heartbeat (10m)

--- config/initializers/timers.rb
+++ config/initializers/timers.rb
@@ -1,18 +1,25 @@
 if Rails.const_defined? :Console
   puts "\e[94mSkipping timers since we're in Rails Console\e[0m"
 else
-  scheduler = Rufus::Scheduler.singleton
-  
-  Houston.config.timers.each do |(interval, name, options, block)|
-    scheduler.every(interval, options.merge(tag: name)) do |job|
-      Rails.logger.debug "\e[34m[#{job.tags.first}/#{job.original}] Running job\e[0m"
-      begin
-        block.call
-      rescue
-        Rails.logger.warn "\e[31m[#{job.tags.first}/#{job.original}] \e[1m#{$!.message}\e[0m"
-        Houston.report_exception($!, job_name: job.tags.first, job_id: job.id) rescue nil
-      ensure
-        ActiveRecord::Base.clear_active_connections!
+  scheduler = Rufus::Scheduler.singleton(lockfile: Rails.root.join(".rufus-scheduler.lock").to_s)
+  if scheduler.down?
+    raise "Rufus::Scheduler cannot be started; it is already running in another process!"
+  else
+    scheduler.every("10s") do |job|
+      Rails.logger.debug "\e[34m[timer/#{job.original}] Alive\e[0m"
+    end
+    
+    Houston.config.timers.each do |(interval, name, options, block)|
+      scheduler.every(interval, options.merge(tag: name)) do |job|
+        Rails.logger.debug "\e[34m[#{job.tags.first}/#{job.original}] Running job\e[0m"
+        begin
+          block.call
+        rescue
+          Rails.logger.warn "\e[31m[#{job.tags.first}/#{job.original}] \e[1m#{$!.message}\e[0m"
+          Houston.report_exception($!, job_name: job.tags.first, job_id: job.id) rescue nil
+        ensure
+          ActiveRecord::Base.clear_active_connections!
+        end
       end
     end
   end
