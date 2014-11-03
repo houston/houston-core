7a700eaf78ef6d8c3828bc65898c3085c81f4e84
Bob Lail <bob.lail@cph.org>
2014-11-03 17:54:20 -0600

    [skip] Recorded the process which owns the scheduler thread in a pidfile (10m)

--- config/initializers/timers.rb
+++ config/initializers/timers.rb
@@ -4,15 +4,22 @@ if Rails.const_defined? :Console
   puts "\e[94mSkipping timers since we're in Rails Console\e[0m"
 else
   
-  lockfile = Rails.root.join(".rufus-scheduler.lock").to_s
+  lockfile = Rails.root.join(".rufus-scheduler.lock").to_s.freeze
+  pidfile = Rails.root.join("tmp/pids/scheduler").to_s.freeze
+  
   $scheduler = Rufus::Scheduler.new(lockfile: lockfile)
   if $scheduler.up?
     Rails.logger.info "\e[34m[scheduler:boot] Starting scheduler on process #{$$}\e[0m"
+    FileUtils.mkdir_p Rails.root.join("tmp/pids")
+    File.open(pidfile, "w") { |f| f.write($$) }
     Houston.observer.fire "scheduler:boot"
     
     at_exit do
-      Rails.logger.warn "\e[34m[scheduler:shutdown] Stopping scheduler on process #{$$}\e[0m"
-      Houston.observer.fire "scheduler:shutdown"
+      if !FileTest.exists?(pidfile) || File.read(pidfile).to_i == $$
+        Rails.logger.warn "\e[34m[scheduler:shutdown] Stopping scheduler on process #{$$}\e[0m"
+        File.delete(pidfile)
+        Houston.observer.fire "scheduler:shutdown"
+      end
     end
     
     Houston.config.timers.each do |(interval, name, options, block)|
