976eaf965774076f3ed9479c35d6e2758fe6c63d
Bob Lail <bob.lail@cph.org>
2014-11-30 19:41:47 -0600

    [skip] Removed unnecessary attempt to load Unfuddle plugin (1m)

--- config/initializers/requirements.rb
+++ config/initializers/requirements.rb
@@ -1,9 +1,7 @@
 $:.unshift Rails.root.join("lib", "freight_train", "lib").to_s
 $:.unshift Rails.root.join("lib", "lail_extensions", "lib").to_s
-$:.unshift Rails.root.join("lib", "unfuddle", "lib").to_s
 
 require 'freight_train'
 require 'lail/core_extensions'
-require 'unfuddle'
 require 'parallel_enumerable'
 require 'raise_errors'
