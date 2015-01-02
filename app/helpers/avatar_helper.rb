0ba8bac803e71cc7c585ced38cc5b8e3874a94d7
Bob Lail <bob.lail@cph.org>
2015-01-01 18:09:12 -0600

    [refactor] Pulled doubling of gravatar image sizes out of `gravatar_url` helper (3m)

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -6,7 +6,7 @@ module AvatarHelper
     return "" unless user
     
     size = options.fetch(:size, 24)
-    "<img class=\"avatar user-#{user.id}\" src=\"#{gravatar_url(user.email, options)}\" width=\"#{size}\" height=\"#{size}\" alt=\"#{user.name}\" />".html_safe
+    "<img class=\"avatar user-#{user.id}\" src=\"#{gravatar_url(user.email, size: size * 2)}\" width=\"#{size}\" height=\"#{size}\" alt=\"#{user.name}\" />".html_safe
   end
   
   
@@ -15,7 +15,7 @@ module AvatarHelper
     
     size = options.fetch(:size, 24)
     alt = options[:alt]
-    "<img class=\"avatar\" src=\"#{gravatar_url(email, options)}\" width=\"#{size}\" height=\"#{size}\" alt=\"#{alt}\" />".html_safe
+    "<img class=\"avatar\" src=\"#{gravatar_url(email, size: size * 2)}\" width=\"#{size}\" height=\"#{size}\" alt=\"#{alt}\" />".html_safe
   end
   
   
@@ -23,7 +23,7 @@ module AvatarHelper
   # http://en.gravatar.com/site/implement/url
   def gravatar_url(email, options={})
     url = "http://www.gravatar.com/avatar/#{Digest::MD5::hexdigest(email)}?r=g&d=retro"
-    url << "&s=#{options[:size] * 2}" if options.key?(:size)
+    url << "&s=#{options[:size]}" if options.key?(:size)
     url
   end
   
