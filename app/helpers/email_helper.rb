6553ba4c2b16d3456cb0a3992449577a0bd2a2ee
Bob Lail <bob.lail@cph.org>
2015-01-01 18:09:43 -0600

    [skip] Stopped escaping valid CSS (5m)

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -3,7 +3,7 @@ module EmailHelper
   def render_scss(relative_path)
     asset = Rails.application.assets.find_asset(relative_path)
     raise "Asset not found #{relative_path.inspect}" unless asset
-    asset.to_s
+    asset.to_s.html_safe
   end
   
   def for_email?
