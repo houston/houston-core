0b6cfa3a90dc56f2cb439a959e7cd0738b5f6b97
Bob Lail <bob.lail@cph.org>
2015-01-08 11:38:22 -0600

    [improvement] Corrected a few values and added debug output to reports (40m)

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -3,14 +3,23 @@ class Measurement < ActiveRecord::Base
   belongs_to :subject, polymorphic: true
   
   validates :name, :value, :taken_at, presence: true
+  validates :name, length: { maximum: 50 }
   
   default_scope -> { order(arel_table[:taken_at].desc) }
   
   class << self
     def take!(attributes)
-      identifying_attributes = attributes.pick(:subject_type, :subject_id, :taken_at, :name)
+      required_keys = [:subject_type, :subject_id, :taken_at, :name].freeze
+      
+      identifying_attributes = attributes.pick(required_keys)
       subject = attributes[:subject]
       identifying_attributes.merge!(subject_type: subject.class.name, subject_id: subject.id) if subject
+      identifying_attributes.reverse_merge!(subject_type: nil, subject_id: nil)
+      
+      required_keys.each do |key|
+        raise ArgumentError, "#{key.inspect} is required to take a measurement" unless identifying_attributes.key?(key)
+      end
+      
       find_or_initialize_by(identifying_attributes).tap do |measurement|
         measurement.value = attributes[:value]
         measurement.save!
@@ -33,8 +42,17 @@ class Measurement < ActiveRecord::Base
       where(arel_table[:taken_on].gteq(date))
     end
     
+    def taken_between(date0, date1)
+      taken_after(date0).taken_before(date1)
+    end
+    
     def for(subject)
+      return where(subject_type: nil, subject_id: nil) if subject.nil?
       where(subject_type: subject.class.name, subject_id: subject.id)
+    end
+    
+    def global
+      self.for(nil)
     end
     
     # Valid identifies for names
@@ -55,23 +73,27 @@ class Measurement < ActiveRecord::Base
       pluck(:value).inject(0) { |sum, value| sum + value.to_d }
     end
     
+    def value
+      limit(1).pluck(:value)[0]
+    end
+    
     def mean
       denominator = count
       return nil if denominator.zero?
       total.to_f / denominator
     end
     
-    def debug
+    def debug(colored: true)
       format_subject = ->(s) { s.is_a?(User) ? s.first_name : s.is_a?(Project) ? s.slug : "" }
-      puts includes(:subject).reorder("taken_at ASC, subject_type ASC, subject_id ASC, name ASC").map { |m|
+      includes(:subject).reorder("taken_at ASC, subject_type ASC, subject_id ASC, name ASC").map { |m|
         line = [ m.taken_on.strftime("%-m/%-d").rjust(5),
                  m.taken_at.strftime("%H:%M:%S"),
                  format_subject[m.subject].ljust(9),
-                 m.name.ljust(40),
+                 m.name.ljust(50),
                  m.value.rjust(8) ].join(" ")
-        line = "\e[36m#{line}\e[0m" if m.subject_type == "User"
-        line = "\e[35m#{line}\e[0m" if m.subject_type == "Project"
-        line }
+        line = "\e[36m#{line}\e[0m" if colored && m.subject_type == "User"
+        line = "\e[35m#{line}\e[0m" if colored && m.subject_type == "Project"
+        line }.join("\n")
     end
   end
   
