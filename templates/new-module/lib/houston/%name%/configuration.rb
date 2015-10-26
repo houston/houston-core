e7d770f51c36e4ef2d8b5ff3bb34338854dc5e66
Bob Lail <bob.lail@cph.org>
2015-10-25 20:56:48 -0500

    [fix] Made a few corrections to generated modules (3m)

--- templates/new-module/lib/houston/%name%/configuration.rb
+++ templates/new-module/lib/houston/%name%/configuration.rb
@@ -1,11 +1,6 @@
 module Houston::<%= camelized %>
   class Configuration
 
-    def initialize
-      config = Houston.config.module(:<%= name %>).config
-      instance_eval(&config) if config
-    end
-
     # Define configuration DSL here
 
   end
