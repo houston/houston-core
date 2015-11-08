9d15dad3fb5ba710e99a08f6c7562c6256cd481f
Bob Lail <bob.lail@cph.org>
2015-11-08 16:30:33 -0600

    [skip] Added validations to Github::PullRequest (1m)

--- app/models/github/pull_request.rb
+++ app/models/github/pull_request.rb
@@ -17,8 +17,7 @@ module Github
     after_create { Houston.observer.fire "github:pull:opened", self }
     after_update { Houston.observer.fire "github:pull:updated", self, changes }
 
-    validates :project_id, :title, :number, :repo, :url, :base_ref, :base_sha, :head_ref, :head_sha,
-      presence: true
+    validates :project_id, :title, :number, :repo, :url, :base_ref, :base_sha, :head_ref, :head_sha, :username, presence: true
     validates :number, uniqueness: { scope: :project_id }
 
     class << self
