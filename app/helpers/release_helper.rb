07b8b7c62f3cf1b177cbcadf307de4659dc29fdc
Bob Lail <bob.lailfamily@gmail.com>
2015-12-02 19:49:32 -0600

    [skip] Added metadata to Release notes so that they can be unfurled (15m)

--- app/helpers/release_helper.rb
+++ app/helpers/release_helper.rb
@@ -4,6 +4,17 @@ module ReleaseHelper
     "<span class=\"weekday\">#{date.strftime("%A")}</span> #{date.strftime("%b %e, %Y")}".html_safe
   end
 
+  def format_release_subject(release)
+    date = release.date.strftime("%b %e")
+    "Release to #{release.environment_name} (#{date})"
+  end
+
+  def format_release_description(release)
+    ordered_by_tag(release.release_changes).map do |change|
+      "*#{change.tag.name}*   #{change.description}"
+    end.join("\n")
+  end
+
   def ordered_by_tag(changes)
     changes.sort_by { |change| change.tag ? change.tag.position : 99 }
   end
