57242effb689e580dcca2b530060698ab18c7e7b
Bob Lail <bob.lailfamily@gmail.com>
2015-12-13 17:43:33 -0600

    [improvement] Allowed filtering the list of Pull Requests (30m)

--- app/controllers/github/pulls_controller.rb
+++ app/controllers/github/pulls_controller.rb
@@ -1,7 +1,11 @@
 class Github::PullsController < ApplicationController
 
   def index
-    @pulls = Github::PullRequest.order(created_at: :desc)
+    @pulls = Github::PullRequest.order(created_at: :desc).preload(:project, :user)
+    @labels = @pulls.flat_map(&:labels).uniq { |label| label["name"] }.sort_by { |label| label["name"] }
+    @selected_labels = params.fetch(:only, "").split(/,\s*/)
+    @selected_labels = @labels.map { |label| label["name"] } if @selected_labels.none?
+    @selected_labels -= params.fetch(:except, "").split(/,\s*/)
     @title = "Pull Requests (#{@pulls.count})"
   end
 
