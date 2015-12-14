d9f03d0ced81c3428a1ca9e62d6f0b6daeeb3910
Bob Lail <bob.lailfamily@gmail.com>
2015-12-13 20:49:58 -0600

    [skip] Showed dark text on light green labels (1m)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -78,7 +78,7 @@ module ApplicationHelper
   def pull_request_label(label)
     background = "##{label["color"]}"
     foreground = "#fff"
-    foreground = "#333" if %w{#f7c6c7 #d4c5f9 #fbca04 #fad8c7}.member? background
+    foreground = "#333" if %w{#f7c6c7 #d4c5f9 #fbca04 #fad8c7 #bfe5bf}.member? background
     "<span class=\"label\" style=\"background: #{background}; color: #{foreground};\">#{label["name"]}</span>".html_safe
   end
 
