c586a706f1f1cc7d5d625a4259c543e4a3272692
Bob Lail <bob.lailfamily@gmail.com>
2016-05-23 20:47:25 -0500

    [skip] Stopped swallowing exceptions in development and test environments (1m)

--- config/initializers/houston_report_exception.rb
+++ config/initializers/houston_report_exception.rb
@@ -3,19 +3,21 @@ module Houston
   def self.report_exception(exception, other_data={})
     raise if Rails.env.test? || Rails.env.development?
 
-    Rails.logger.error "#{exception.class}: #{exception.message}\n#{exception.backtrace.join("\n  ")}"
+    begin
+      Rails.logger.error "#{exception.class}: #{exception.message}\n#{exception.backtrace.join("\n  ")}"
 
-    if defined?(Airbrake)
-      other_data[:parameters] ||= {}
-      case exception
-      when Faraday::HTTP::Error
-        other_data[:parameters].merge!(_normalize_faraday_env(exception.env))
+      if defined?(Airbrake)
+        other_data[:parameters] ||= {}
+        case exception
+        when Faraday::HTTP::Error
+          other_data[:parameters].merge!(_normalize_faraday_env(exception.env))
+        end
+        other_data[:parameters].merge!(exception.additional_information)
+        Airbrake.notify(exception, other_data)
       end
-      other_data[:parameters].merge!(exception.additional_information)
-      Airbrake.notify(exception, other_data)
+    rescue Exception => e
+      Rails.logger.error "\e[31;1mAn error occurred reporting the exception: \e[0;31m#{e.class}: #{e.message}\n   #{e.backtrace.join("\n  ")}\e[0m"
     end
-  rescue Exception => e
-    Rails.logger.error "\e[31;1mAn error occurred reporting the exception: \e[0;31m#{e.class}: #{e.message}\n   #{e.backtrace.join("\n  ")}\e[0m"
   end
 
   def self._normalize_faraday_env(env)
