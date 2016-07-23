3a04aa2e01e46f4e73152b32e34e53efee42eb5a
Bob Lail <bob.lailfamily@gmail.com>
2016-07-23 15:57:15 -0500

    [improvement] Formatted (and collapsed) Action params (40m)

--- app/helpers/actions_helper.rb
+++ app/helpers/actions_helper.rb
@@ -1,7 +1,42 @@
 module ActionsHelper
 
   def format_action_params(params)
-    Houston::ParamsSerializer.new.dump params
+    return "<pre>{}</pre>".html_safe if params == {}
+    <<-HTML.html_safe
+      <div class="action-params-short">
+        <pre>{ #{params.keys.map(&:inspect).join(", ")} }</pre>
+      </div>
+      <div class="action-params-full">
+        <pre>#{_add_white_space Houston::ParamsSerializer.new.dump(params)}</pre>
+      </div>
+    HTML
+  end
+
+  def _add_white_space(json)
+    scanner = StringScanner.new(json)
+    output = ""
+    indent = 0
+    until scanner.eos?
+      match = scanner.scan(/(?:[\[\]\{\}":,]|[^\[\]\{\}":,]+)/)
+      case match
+      when "{", "["
+        indent += 2
+        output << "#{match}\n#{" " * indent}"
+      when "}", "]"
+        indent -= 2
+        output << "\n#{" " * indent}#{match}"
+      when "\""
+        # hopefully this grabs the entire string
+        output << "\"" << scanner.scan(/.*?(?<!\\)"/)
+      when ":"
+        output << ": "
+      when ","
+        output << ",\n#{" " * indent}"
+      else
+        output << match
+      end
+    end
+    output
   end
 
 end
