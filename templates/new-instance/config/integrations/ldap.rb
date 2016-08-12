022cee343e3b6dbc56c797ec6a36422d81ecda2d
Bob Lail <bob.lailfamily@gmail.com>
2016-08-12 17:46:03 -0500

    [refactor] Made instructions an optional configuration value for authentication strategies (1m)

--- templates/new-instance/config/integrations/ldap.rb
+++ templates/new-instance/config/integrations/ldap.rb
@@ -1,4 +1,5 @@
 # Houston.config.authentication_strategy :ldap do
+#   instructions "You can log in with your corporate username and password"
 #   host "10.5.3.100"
 #   port 636
 #   ssl :simple_tls
