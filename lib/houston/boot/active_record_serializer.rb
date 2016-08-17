365e3ac337a66115629b331196fff22170abffd2
Bob Lail <bob.lailfamily@gmail.com>
2016-08-16 20:33:24 -0500

    [fix] Fixed serializing action params (8m)

--- lib/houston/boot/active_record_serializer.rb
+++ lib/houston/boot/active_record_serializer.rb
@@ -8,8 +8,8 @@ module Houston
     def pack(record)
       model = record.class
       normal_attributes = record.attributes.each_with_object({}) do |(attribute, value), attributes|
-        attributes[attribute] = value.nil? ? nil :
-          model.type_for_attribute(attribute).type_cast_for_schema(value)
+        column = model.column_for_attribute(attribute)
+        attributes[attribute] = model.connection.type_cast_from_column(column, value)
       end
       { "class" => model.name, "attributes" => normal_attributes }
     end
