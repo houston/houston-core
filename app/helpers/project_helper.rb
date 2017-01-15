17a83852426cc7a4eb04193c34d3b67b196fe9c6
Bob Lail <bob.lailfamily@gmail.com>
2017-01-15 12:04:21 -0600

    [skip] Removed the “Last Commit” column from the Projects view (2m)

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,13 +1,5 @@
 module ProjectHelper
 
-  def with_most_recent_commit(project)
-    commit = project.head
-    if commit
-      commit.project = project # so that _Commit_ doesn't load project again
-      yield commit
-    end
-  end
-
   def project_label(project)
     return '<b class="label unknown">&nbsp;</b>'.html_safe unless project
     "<b class=\"label #{project.color}\">#{h project.slug.gsub("_", " ")}</b>".html_safe
