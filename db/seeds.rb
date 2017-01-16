fefa8c5d888215d9caf1b15d2e0ef02fc3e8dcab
Bob Lail <bob.lailfamily@gmail.com>
2017-01-15 21:50:02 -0600

    [skip] Deleted unused seed files (1m)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,2 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-
-require 'active_record/fixtures'
-
-path = Rails.root.join("db", "fixtures").to_s
-tables = Dir.entries(path).map { |file| file[/^.*(?=\.yml$)/] }.compact
-
-ActiveRecord::Fixtures.create_fixtures(path, tables)
