94078dd3004b66d6dca24df3f3b452eed9c04a0c
Bob Lail <bob.lailfamily@gmail.com>
2017-01-16 11:35:51 -0600

    [skip] Extracted migration linking tasks and commits to Houston::Tickets (2m)

--- db/migrate/20140515200824_link_tasks_and_commits.rb
+++
@@ -1,8 +0,0 @@
-class LinkTasksAndCommits < ActiveRecord::Migration
-  def change
-    create_table :commits_tasks, :id => false do |t|
-      t.references :commit, :task
-      t.index [:commit_id, :task_id], :unique => true
-    end
-  end
-end
