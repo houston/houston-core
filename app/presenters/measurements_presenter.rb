cb5be096da33dec17086b86f93ad31ee3b3879e4
Bob Lail <bob.lailfamily@gmail.com>
2017-01-28 11:45:05 -0600

    [skip] Optimized the Measurements API using the pluck-json technique (10m)

--- app/presenters/measurements_presenter.rb
+++ app/presenters/measurements_presenter.rb
@@ -7,19 +7,14 @@ class MeasurementsPresenter
 
   def to_json(*args)
     Houston.benchmark "[#{self.class.name.underscore}] Render JSON" do
-      MultiJson.dump(as_json(*args))
-    end
-  end
+      query = measurements.select(<<-SQL)
+        taken_at "timestamp",
+        name,
+        value,
+        json_build_object('type', subject_type, 'id', subject_id) "subject"
+      SQL
 
-  def as_json(*args)
-    Houston.benchmark "[#{self.class.name.underscore}] Prepare JSON" do
-      measurements.pluck(:name, :taken_at, :value, :subject_type, :subject_id)
-        .map do |name, timestamp, value, subject_type, subject_id|
-        { timestamp: timestamp,
-          name: name,
-          value: value,
-          subject: { type: subject_type, id: subject_id } }
-      end
+      ActiveRecord::Base.connection.select_value("select array_to_json(array_agg(row_to_json(t))) from (#{query.to_sql}) t")
     end
   end
 
