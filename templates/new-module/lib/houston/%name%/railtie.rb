90a4cc25e7ab2fe257ab47cb818ffa36915c9c9b
Bob Lail <bob.lailfamily@gmail.com>
2017-02-18 12:28:00 -0600

    [skip] Showed how to extend core models with commented-out code in modules’ railties (2m)

--- templates/new-module/lib/houston/%name%/railtie.rb
+++ templates/new-module/lib/houston/%name%/railtie.rb
@@ -5,6 +5,10 @@ module Houston
       # The block you pass to this method will run for every request
       # in development mode, but only once in production.
       # config.to_prepare do
+      #
+      #   Add methods or relationships to a core model (like User)
+      #   by including a module that adds the concern here:
+      #   ::User.send :include, Houston::<%= camelized %>::UserExt
       # end
 
     end
