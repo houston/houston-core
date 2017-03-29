51b6ad04d77decc226ec770dd6de8901ee52026b
Bob Lail <bob.lailfamily@gmail.com>
2017-03-28 23:25:25 -0500

    [skip] Dropped layouts/naked (4m)
    It is the same as layouts/application but without navigation

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,5 +1,10 @@
 module LayoutHelper
 
+  def layout_show_navigation?
+    return true unless instance_variable_defined?(:@_layout_show_navigation)
+    @_layout_show_navigation
+  end
+
   def layout_container_fluid?
     return true unless instance_variable_defined?(:@_layout_container_fluid)
     @_layout_container_fluid
