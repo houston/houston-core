d9c2e73d34e9e653d1b0fc2271dc1d82280846cc
Bob Lail <bob.lailfamily@gmail.com>
2017-04-14 13:58:48 -0500

    [refactor] Moved serializer requirement to boot (1m)

--- lib/houston/boot.rb
+++ lib/houston/boot.rb
@@ -1,6 +1,7 @@
 require "houston/boot/configuration"
 require "houston/boot/running_as"
 require "houston/boot/daemonize"
+require "houston/boot/serializer"
 require "houston/boot/extensions"
 
 require "houston/adapters"
