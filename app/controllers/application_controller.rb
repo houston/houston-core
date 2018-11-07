9e466f1aab0173ff676d3337dd48819b4d8d890e
Bob Lail <bob.lailfamily@gmail.com>
2018-11-07 07:51:58 -0600

    [skip] Authenticated via token _after_ verifying CSRF token to work around a bug where CSRF tokens were always failing (6m)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token, if: :authenticated_via_token?
 
-  prepend_before_action :token_authenticate!
+  before_action :token_authenticate!
   around_action :with_current_project
 
 
